3b5b7a6956a02062569446b743779f62e143e6d5
notargs <satoyutaka94@gmail.com>
2019-01-27 07:40:32 +0900

    Add game sounds

--- Assets/GGJ/Scripts/Mother.cs
+++ Assets/GGJ/Scripts/Mother.cs
@@ -9,10 +9,11 @@ using UnityEngine.AI;
 public class Mother : MonoBehaviour
 {
     [Inject] Anger anger;
+    [SerializeField] Collider trigger;
     
     void Start()
     {
-        this.OnTriggerStayAsObservable()
+        trigger.OnTriggerStayAsObservable()
             .Select(collider => collider.GetComponent<Player>())
             .Where(player => player != null)
             .Where(player => player.State.Value != PlayerState.Studying)
@@ -21,8 +22,8 @@ public class Mother : MonoBehaviour
 
     public void Initialize(MotherSpawnPoint point, int index)
     {
-        transform.position = point.Position;
         var agent = GetComponent<NavMeshAgent>();
+        agent.Warp(point.Position);
         agent.avoidancePriority = 30 + index;
     }
 }
--- Assets/GGJ/Scripts/npc/RoomPatrol.cs
+++ Assets/GGJ/Scripts/npc/RoomPatrol.cs
@@ -16,11 +16,6 @@ public class RoomPatrol : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
 
-        // Disabling auto-braking allows for continuous movement
-        // between points (ie, the agent doesn't slow down as it
-        // approaches a destination point).
-        agent.autoBraking = false;
-
         GotoNextPoint();
     }
 
