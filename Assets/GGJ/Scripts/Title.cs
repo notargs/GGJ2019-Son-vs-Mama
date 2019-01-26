eb16f23582254203f81bbec721fef735a00b2874
notargs <satoyutaka94@gmail.com>
2019-01-26 13:27:57 +0900

    Add gameover & title system

---
+++ Assets/GGJ/Scripts/Title.cs
@@ -0,0 +1,17 @@
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class Title : MonoBehaviour
+{
+    [Inject] ZenjectSceneLoader sceneLoader;
+    
+    [SerializeField] Button button = default;
+    
+    void Start()
+    {
+        button.OnClickAsObservable()
+            .Subscribe(_ => sceneLoader.LoadScene("MainScene"));
+    }
+}
