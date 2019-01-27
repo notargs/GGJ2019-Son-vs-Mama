c9386edd53f2caf43d430cc2e6642315e4c5fad6
SyouheTomita <ne_ke_bd_se_17@icloud.com>
2019-01-27 14:40:26 +0900

    exit button create

--- Assets/GGJ/Scripts/Title.cs
+++ Assets/GGJ/Scripts/Title.cs
@@ -10,11 +10,26 @@ namespace GGJ.Scripts
         [Inject] ZenjectSceneLoader sceneLoader;
     
         [SerializeField] Button button = default;
+        [SerializeField] Button exit = default;
     
         void Start()
         {
             button.OnClickAsObservable()
                 .Subscribe(_ => sceneLoader.LoadScene("MainScene"));
+            exit.OnClickAsObservable()
+                .Subscribe(_ => GameEnd());
+        }
+
+       public void GameEnd()
+        {
+#if UNITY_EDITOR
+
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+		 exit.OnClickAsObservable()
+                .Subscribe(_=>Application.Quit());
+#endif
+
         }
     }
 }
